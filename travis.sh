6e43175bc8a1951968acbebddc2f8c0b6ed09f40
Sergio Kopplin <sergiokopplin@gmail.com>
2016-01-22 00:51:21 -0200

    broken travis fixed

--- travis.sh
+++ travis.sh
@@ -2,6 +2,5 @@
 set -e # halt script on error
 
 echo 'Testing travis...'
-bundle exec travis-lint
 bundle exec jekyll build
 bundle exec htmlproof ./_site --verbose --href-ignore https://packagecontrol.io/packages/Theme%20-%20Carmesim
