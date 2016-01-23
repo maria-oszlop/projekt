812aa3637f5ebe5ab976437c4bbd208cd2cc98f4
Sergio Kopplin <sergiokopplin@gmail.com>
2016-01-23 11:12:42 -0200

    no more ignores for travis

--- travis.sh
+++ travis.sh
@@ -3,4 +3,4 @@ set -e # halt script on error
 
 echo 'Testing travis...'
 bundle exec jekyll build
-bundle exec htmlproof ./_site --verbose --href-ignore https://packagecontrol.io/packages/Theme%20-%20Carmesim
+bundle exec htmlproof ./_site --verbose
